559932c4c7d54eb4bb97e0e56f4106c58bd92c49
BigBoss2310 <2153797@slu.edu.ph>
2017-05-10 23:04:23 +0800

    Database
    modified the categories and services tables

--- database.sql
+++ database.sql
@@ -1,58 +1,57 @@
-CREATE DATABASE  IF NOT EXISTS `database` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `database`;
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: database
--- ------------------------------------------------------
--- Server version	5.7.14
+-- Host: 127.0.0.1
+-- Generation Time: May 10, 2017 at 03:02 PM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category_service`
+--
+
+CREATE TABLE `category_service` (
+  `category_id` int(11) NOT NULL,
+  `category_name` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `customer`
 --
 
-DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `customer` (
   `customer_id` int(11) NOT NULL,
   `customer_name` varchar(100) NOT NULL,
   `subject` varchar(100) NOT NULL,
   `scheduled_time` datetime NOT NULL,
   `scheduled_day` varchar(100) NOT NULL,
-  `account_id` int(11) NOT NULL,
-  PRIMARY KEY (`customer_id`),
-  KEY `account_id_idx` (`account_id`),
-  CONSTRAINT `account_id` FOREIGN KEY (`account_id`) REFERENCES `customer_register` (`account_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `account_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `customer`
---
-
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `customer_register`
 --
 
-DROP TABLE IF EXISTS `customer_register`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `customer_register` (
   `account_id` int(11) NOT NULL,
   `account_role` varchar(45) NOT NULL,
@@ -61,78 +60,30 @@ CREATE TABLE `customer_register` (
   `age` int(11) NOT NULL,
   `contact_number` int(11) NOT NULL,
   `gender` varchar(45) NOT NULL,
-  `home_address` varchar(100) NOT NULL,
-  PRIMARY KEY (`account_id`)
+  `home_address` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `customer_register`
---
-
-LOCK TABLES `customer_register` WRITE;
-/*!40000 ALTER TABLE `customer_register` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customer_register` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `educational_services`
---
-
-DROP TABLE IF EXISTS `educational_services`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `educational_services` (
-  `service_id` int(11) NOT NULL,
-  `description` varchar(250) NOT NULL,
-  `sp_id` int(11) NOT NULL,
-  PRIMARY KEY (`service_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `educational_services`
---
-
-LOCK TABLES `educational_services` WRITE;
-/*!40000 ALTER TABLE `educational_services` DISABLE KEYS */;
-/*!40000 ALTER TABLE `educational_services` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `payment`
 --
 
-DROP TABLE IF EXISTS `payment`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `payment` (
   `sp_id` int(11) NOT NULL,
   `customer_id` int(11) NOT NULL,
   `date_started` varchar(100) NOT NULL,
   `date_ended` varchar(100) NOT NULL,
   `service` varchar(100) NOT NULL,
-  `request_id` int(11) NOT NULL,
-  PRIMARY KEY (`sp_id`,`customer_id`)
+  `request_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `payment`
---
-
-LOCK TABLES `payment` WRITE;
-/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
-/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `request`
 --
 
-DROP TABLE IF EXISTS `request`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `request` (
   `request_id` int(11) NOT NULL,
   `request_name` varchar(100) NOT NULL,
@@ -140,29 +91,15 @@ CREATE TABLE `request` (
   `requested_time` datetime NOT NULL,
   `requested_day` varchar(100) NOT NULL,
   `request_status` varchar(45) NOT NULL,
-  `customer_id` int(11) NOT NULL,
-  PRIMARY KEY (`request_id`),
-  KEY `customer_id_idx` (`customer_id`),
-  CONSTRAINT `customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `customer_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `request`
---
-
-LOCK TABLES `request` WRITE;
-/*!40000 ALTER TABLE `request` DISABLE KEYS */;
-/*!40000 ALTER TABLE `request` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `service`
 --
 
-DROP TABLE IF EXISTS `service`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `service` (
   `service_id` int(11) NOT NULL,
   `service_name` varchar(100) NOT NULL,
@@ -174,31 +111,26 @@ CREATE TABLE `service` (
   `service_price` int(11) NOT NULL,
   `payment_status` varchar(45) NOT NULL,
   `request_id` int(11) NOT NULL,
-  `sp_id` int(11) NOT NULL,
-  PRIMARY KEY (`service_id`),
-  KEY `request_id_idx` (`request_id`),
-  KEY `sp_id_idx` (`sp_id`),
-  CONSTRAINT `request_id` FOREIGN KEY (`request_id`) REFERENCES `request` (`request_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `sp_id` FOREIGN KEY (`sp_id`) REFERENCES `service_provider` (`sp_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  `sp_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `service`
+-- Table structure for table `services_offered`
 --
 
-LOCK TABLES `service` WRITE;
-/*!40000 ALTER TABLE `service` DISABLE KEYS */;
-/*!40000 ALTER TABLE `service` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `services_offered` (
+  `service_id` int(11) NOT NULL,
+  `service_name` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `service_provider`
 --
 
-DROP TABLE IF EXISTS `service_provider`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `service_provider` (
   `sp_id` int(11) NOT NULL,
   `sp_name` varchar(100) NOT NULL,
@@ -206,27 +138,27 @@ CREATE TABLE `service_provider` (
   `home_address` varchar(100) NOT NULL,
   `email_address` varchar(100) NOT NULL,
   `specialization` varchar(100) NOT NULL,
-  `account_id` int(11) NOT NULL,
-  PRIMARY KEY (`sp_id`)
+  `account_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `service_provider`
+-- Table structure for table `special_workshop`
 --
 
-LOCK TABLES `service_provider` WRITE;
-/*!40000 ALTER TABLE `service_provider` DISABLE KEYS */;
-/*!40000 ALTER TABLE `service_provider` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `special_workshop` (
+  `service_id` int(11) NOT NULL,
+  `description` varchar(250) NOT NULL,
+  `sp_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `sp_register`
 --
 
-DROP TABLE IF EXISTS `sp_register`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sp_register` (
   `account_id` int(11) NOT NULL,
   `account_role` varchar(45) NOT NULL,
@@ -238,99 +170,114 @@ CREATE TABLE `sp_register` (
   `gender` varchar(45) NOT NULL,
   `home_address` varchar(100) NOT NULL,
   `service` varchar(100) NOT NULL,
-  `specialization` varchar(100) NOT NULL,
-  PRIMARY KEY (`account_id`)
+  `specialization` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `sp_register`
+-- Indexes for dumped tables
 --
 
-LOCK TABLES `sp_register` WRITE;
-/*!40000 ALTER TABLE `sp_register` DISABLE KEYS */;
-/*!40000 ALTER TABLE `sp_register` ENABLE KEYS */;
-UNLOCK TABLES;
+--
+-- Indexes for table `category_service`
+--
+ALTER TABLE `category_service`
+  ADD PRIMARY KEY (`category_id`);
 
 --
--- Table structure for table `special_workshop`
+-- Indexes for table `customer`
 --
-
-DROP TABLE IF EXISTS `special_workshop`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `special_workshop` (
-  `service_id` int(11) NOT NULL,
-  `description` varchar(250) NOT NULL,
-  `sp_id` int(11) NOT NULL,
-  PRIMARY KEY (`service_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`),
+  ADD KEY `account_id_idx` (`account_id`);
 
 --
--- Dumping data for table `special_workshop`
+-- Indexes for table `customer_register`
 --
-
-LOCK TABLES `special_workshop` WRITE;
-/*!40000 ALTER TABLE `special_workshop` DISABLE KEYS */;
-/*!40000 ALTER TABLE `special_workshop` ENABLE KEYS */;
-UNLOCK TABLES;
+ALTER TABLE `customer_register`
+  ADD PRIMARY KEY (`account_id`);
 
 --
--- Table structure for table `training_services`
+-- Indexes for table `payment`
 --
-
-DROP TABLE IF EXISTS `training_services`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `training_services` (
-  `service_id` int(11) NOT NULL,
-  `description` varchar(250) NOT NULL,
-  `sp_id` int(11) NOT NULL,
-  PRIMARY KEY (`service_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`sp_id`,`customer_id`);
 
 --
--- Dumping data for table `training_services`
+-- Indexes for table `request`
 --
-
-LOCK TABLES `training_services` WRITE;
-/*!40000 ALTER TABLE `training_services` DISABLE KEYS */;
-/*!40000 ALTER TABLE `training_services` ENABLE KEYS */;
-UNLOCK TABLES;
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`request_id`),
+  ADD KEY `customer_id_idx` (`customer_id`);
 
 --
--- Table structure for table `tutorial_services`
+-- Indexes for table `service`
 --
-
-DROP TABLE IF EXISTS `tutorial_services`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tutorial_services` (
-  `service_id` int(11) NOT NULL,
-  `description` varchar(250) NOT NULL,
-  `sp_id` int(11) NOT NULL,
-  PRIMARY KEY (`service_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`service_id`),
+  ADD KEY `request_id_idx` (`request_id`),
+  ADD KEY `sp_id_idx` (`sp_id`);
 
 --
--- Dumping data for table `tutorial_services`
+-- Indexes for table `services_offered`
+--
+ALTER TABLE `services_offered`
+  ADD PRIMARY KEY (`service_id`);
+
+--
+-- Indexes for table `service_provider`
+--
+ALTER TABLE `service_provider`
+  ADD PRIMARY KEY (`sp_id`);
+
+--
+-- Indexes for table `special_workshop`
+--
+ALTER TABLE `special_workshop`
+  ADD PRIMARY KEY (`service_id`);
+
+--
+-- Indexes for table `sp_register`
+--
+ALTER TABLE `sp_register`
+  ADD PRIMARY KEY (`account_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
-LOCK TABLES `tutorial_services` WRITE;
-/*!40000 ALTER TABLE `tutorial_services` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tutorial_services` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- AUTO_INCREMENT for table `category_service`
+--
+ALTER TABLE `category_service`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `services_offered`
+--
+ALTER TABLE `services_offered`
+  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Constraints for table `customer`
+--
+ALTER TABLE `customer`
+  ADD CONSTRAINT `account_id` FOREIGN KEY (`account_id`) REFERENCES `customer_register` (`account_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `request`
+--
+ALTER TABLE `request`
+  ADD CONSTRAINT `customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `service`
+--
+ALTER TABLE `service`
+  ADD CONSTRAINT `request_id` FOREIGN KEY (`request_id`) REFERENCES `request` (`request_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `sp_id` FOREIGN KEY (`sp_id`) REFERENCES `service_provider` (`sp_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-05-10 20:11:09
